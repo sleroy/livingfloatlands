d5c7744ef9b7f19fa4829a5f51dacfca607eb8b4
Sylvain Leroy <contact@sylvainleroy.com>
2024-09-29 21:33:36 +0200

    fix

--- dye.lua
+++ dye.lua
@@ -1,68 +1,68 @@
 minetest.register_craft({
-	output = "dye:brown 2",
+	output = "mcl_dyes:brown 2",
 	recipe = {
 		{"livingfloatlands:coldsteppe_grass1"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:brown 2",
+	output = "mcl_dyes:brown 2",
 	recipe = {
 		{"livingfloatlands:coldsteppe_grass2"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:black 2",
+	output = "mcl_dyes:dyes:black 2",
 	recipe = {
 		{"livingfloatlands:coldsteppe_grass3"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:yellow 2",
+	output = "mcl_dyes:yellow 2",
 	recipe = {
 		{"livingfloatlands:coldsteppe_bulbouschervil_plant"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:white 2",
+	output = "mcl_dyes:white 2",
 	recipe = {
 		{"livingfloatlands:giantforest_grass"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:blue 2",
+	output = "mcl_dyes:blue 2",
 	recipe = {
 		{"livingfloatlands:giantforest_grass2"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:yellow 2",
+	output = "mcl_dyes:yellow 2",
 	recipe = {
 		{"livingfloatlands:giantforest_grass3"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:brown 2",
+	output = "mcl_dyes:brown 2",
 	recipe = {
 		{"livingfloatlands:giantforest_oaknut"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:cyan 2",
+	output = "mcl_dyes:cyan 2",
 	recipe = {
 		{"livingfloatlands:puzzlegrass"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:cyan 2",
+	output = "mcl_dyes:cyan 2",
 	recipe = {
 		{"livingfloatlands:puzzlegrass_top"}
 	},
